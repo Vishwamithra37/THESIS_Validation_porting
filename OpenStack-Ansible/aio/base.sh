2a2c14eab701cf0918ec19e27b08abd87e88bf5a
Vishwa Mithra <mithravishwa37@gmail.com>
2022-04-23 19:18:21 +0530

    Wallaby

--- OpenStack-Ansible/aio/base.sh
+++ OpenStack-Ansible/aio/base.sh
@@ -2,7 +2,7 @@ apt update
 apt install ca-certificates
 git clone https://opendev.org/openstack/openstack-ansible /opt/openstack-ansible
 cd /opt/openstack-ansible
-git checkout 23.1.0
+git checkout wallaby-em
 scripts/bootstrap-ansible.sh
 export BOOTSTRAP_OPTS="bootstrap_host_public_interface=ens3"
 scripts/bootstrap-aio.sh
@@ -12,4 +12,5 @@ openstack-ansible setup-infrastructure.yml
 openstack-ansible setup-openstack.yml
 cd /opt/openstack-ansible/playbooks
 cd 
-#the rc file is located in home
+
+
--- OpenStack-Ansible/aio/v3.sh
+++ OpenStack-Ansible/aio/v3.sh
@@ -3,22 +3,22 @@ openstack server create \
 --flavor 19178315-27c1-4506-b6a7-7697cbc6d6b5 \
 --image 235d9bfb-7a13-4434-9966-cfc0ae033e79 \
 --security-group TATTA \
---nic net-id=5dbfc090-9d7d-4009-99d9-af055cf6d74c \
+--nic net-id=54b85f5a-081c-4dc1-914f-479732356b6e \
 --key-name ooru ansible
 sleep 15s
-openstack server add floating ip ansible 91.123.203.6
+openstack server add floating ip ansible 91.123.203.36
 sleep 7s
 openstack server add fixed ip \
---fixed-ip-address 10.6.0.30 \
+--fixed-ip-address 192.168.0.10 \
 ansible \
-thesis
+net1
 
 sleep 3s
 eval `ssh-agent`
 ssh-add ./openkey
 rm ~/.ssh/known_hosts
-scp -o StrictHostKeyChecking=no ./base.sh ubuntu@91.123.203.6:~/base.sh                           
+scp -o StrictHostKeyChecking=no ./base.sh ubuntu@91.123.203.36 :~/base.sh                           
 while :
 do
-ssh -o StrictHostKeyChecking=no ubuntu@91.123.203.6
+ssh -o StrictHostKeyChecking=no ubuntu@91.123.203.36 
 done
