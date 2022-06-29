2fbf7c1dcdd0ddfe094fe4392fd3f31977694b78
Vishwa Mithra <mithravishwa37@gmail.com>
2022-06-30 02:17:18 +0530

    jujubi

--- devStack/vase.sh
+++ devStack/vase.sh
@@ -1,4 +1,4 @@
-#Works only in Virtual BOX. Specs: 6GB RAM, 2 Cores, 40 GB HDD, 2 NICs. Provide the ip of the controller..
+#Works only in Virtual BOX. Specs: 6GB RAM, 2 Cores, 40 GB HDD, 2 NICs. Provide the ip of the controller. Using OSI ubuntu 20.04.
 sudo useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
 sudo su - stack
